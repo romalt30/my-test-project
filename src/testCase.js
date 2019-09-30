b16adff58682571856ee191a6d9af3edd3f1fac5
AltierR <romain.altier@etu.umontpellier.fr>
2019-09-30 11:37:13 +0200

    ajout doc

--- src/testCase.js
+++ src/testCase.js
@@ -1,14 +1,24 @@
 'use strict';
-
+/** permet de décomposer un Json */
 class testCase {
+/**
+ * Créer un décomposeur de Json
+ * @param {json} Json à décomposer
+ */
 	constructor(json) {
 		this.json = json;
 	}
-
+/**
+ * Retourne le Json
+ * @returns {json} Json à décomposer
+ */
 	getJson(){
 		return this.json;
 	}
-
+/**
+ * Récupère les requirements du Json
+ * @returns {(Object|boolean)} Les requirements ou false s'il y en a pas
+ */
 	getRequirement(){
 		var obj = JSON.parse(this.json);
 		if (undefined !== obj.requirement) {
@@ -18,6 +28,10 @@ class testCase {
 	}
 
 	// TODO but not like this : https://www.monkeyuser.com/2017/todo/
+/**
+ * Récupère les executions du Json
+ * @returns {(json|boolean)} Les executions ou false s'il y en a pas ou si le format est invalide
+ */
 	getExecutions(){
 		var obj = JSON.parse(this.json);
 
